5e22b2492bc7ccb2ba2405263c3ef84e17074948
nachoai66 <156576176+nachoai66@users.noreply.github.com>
2024-01-24 08:46:49 +0100

    Update carga.py

--- carga.py
+++ carga.py
@@ -1,1 +1,11 @@
+#Cargamos el modelo que previamente hemos desplegado 
+pickled_model = pickle.load(open('model.pkl', 'rb'))
+#Date cuenta de que el modelo lo estas deserializando 
+#Ahora puedes volver a invocar los métodos propios del modelo generado.
+'''
+  Ejemplos de métodos, son:
+    - predict
+    - ......
+'''
 
+pickled_model.predict(X_test)
